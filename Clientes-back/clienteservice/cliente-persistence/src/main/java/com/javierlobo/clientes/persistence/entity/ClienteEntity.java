ca21106101caa09a492837ae3e9b68111d3b7a87
javierlobo <franciscojavier.lobo@gmail.com>
2020-06-29 14:51:04 +0200

    (DEV) Change in field 'createAt'
    - Comentado del código del PrePresist para que no incluyera la fecha al momento previo de guardar los datos
    - Mapeo del campo para que no pueda estar vacio y lance un mensaje de error

--- Clientes-back/clienteservice/cliente-persistence/src/main/java/com/javierlobo/clientes/persistence/entity/ClienteEntity.java
+++ Clientes-back/clienteservice/cliente-persistence/src/main/java/com/javierlobo/clientes/persistence/entity/ClienteEntity.java
@@ -8,12 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity()
@@ -38,14 +38,15 @@ public class ClienteEntity implements Serializable {
 	@Column(name = "email", nullable=false, unique=true)
 	private String email;
 	
+	@NotNull(message = "No puede estar vacío")
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 	
-	@PrePersist
-	public void prePersist() {
-		this.createAt = new Date();
-	}
+//	@PrePersist
+//	public void prePersist() {
+//		this.createAt = new Date();
+//	}
 	
 	public ClienteEntity() {
 		super();
