042520ff24653efdd90a998ec05a4f51c30b2ff7
javierlobo <franciscojavier.lobo@gmail.com>
2020-10-06 20:53:39 +0200

    (DEV) Add LoggerFactory

--- Clientes-back/clienteservice/cliente-rest/src/main/java/com/javierlobo/clientes/rest/services/impl/UploadFileServiceImpl.java
+++ Clientes-back/clienteservice/cliente-rest/src/main/java/com/javierlobo/clientes/rest/services/impl/UploadFileServiceImpl.java
@@ -8,6 +8,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
@@ -18,36 +20,35 @@ import com.javierlobo.clientes.rest.services.IUploadFileService;
 @Service
 public class UploadFileServiceImpl implements IUploadFileService {
 
-	// ToDo: El log no va, hay que averiguar porqué.
-	// private final Logger log = LoggerFactory.getlogger(UploadFileServiceImpl.class);
-	
+	private Logger log = LoggerFactory.getLogger(UploadFileServiceImpl.class);
+
 	private final static String DIRECTORIO_UPLOAD = "uploads";
-	
+
 	private final static String RESOURCES_IMAGES = "src/main/resources/static/images";
-	
+
 	@Override
 	public Resource cargar(String nombreFoto) throws MalformedURLException {
 		Path rutaArchivo = getPath(nombreFoto);
-//		log.info(rutaArchivo.toString());
+		log.info(rutaArchivo.toString());
 
 		Resource recurso = new UrlResource(rutaArchivo.toUri());
 
-		if(!recurso.exists() && !recurso.isReadable()) {
+		if (!recurso.exists() && !recurso.isReadable()) {
 			rutaArchivo = Paths.get(RESOURCES_IMAGES).resolve("notUser64x64.png").toAbsolutePath();
 			recurso = new UrlResource(rutaArchivo.toUri());
-			// log.error("Error no se pudo cargar la imagen: " + nombreFoto);
+			log.error("Error no se pudo cargar la imagen: " + nombreFoto);
 		}
-		
+
 		return recurso;
 	}
 
 	@Override
 	public String copiar(MultipartFile archivo) throws IOException {
-		String nombreArchivo =UUID.randomUUID().toString() + "_" + archivo.getOriginalFilename().replace(" ", "");
+		String nombreArchivo = UUID.randomUUID().toString() + "_" + archivo.getOriginalFilename().replace(" ", "");
 		Path rutaArchivo = getPath(nombreArchivo);
-//		log.info(rutaArchivo.toString());
+		log.info(rutaArchivo.toString());
 		Files.copy(archivo.getInputStream(), rutaArchivo);
-		
+
 		return nombreArchivo;
 	}
 
@@ -56,13 +57,13 @@ public class UploadFileServiceImpl implements IUploadFileService {
 		if (nombreFoto != null && nombreFoto.length() > 0) {
 			Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFoto).toAbsolutePath();
 			File archivoFotoAnterior = rutaFotoAnterior.toFile();
-			
-			if(archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+
+			if (archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
 				archivoFotoAnterior.delete();
 				return true;
 			}
-		} 
-		
+		}
+
 		return false;
 	}
 
