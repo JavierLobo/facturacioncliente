0284d943f0c2b0be108a54e273606d2fd2cc4d46
javierlobo <franciscojavier.lobo@gmail.com>
2020-10-06 21:04:12 +0200

    (DEV) Add CommandLineRunner funtionality

--- Clientes-back/clienteservice/cliente-rest/src/main/java/com/javierlobo/clientes/rest/ClienteApplication.java
+++ Clientes-back/clienteservice/cliente-rest/src/main/java/com/javierlobo/clientes/rest/ClienteApplication.java
@@ -1,19 +1,38 @@
 package com.javierlobo.clientes.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 //@ComponentScan
 @ComponentScan({"com.javierlobo.clientes"})
 @EntityScan("com.javierlobo.clientes")
 @EnableJpaRepositories("com.javierlobo.clientes")
-public class ClienteApplication {
+public class ClienteApplication implements CommandLineRunner {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println(args);
+		
+		String password = "12345";
+		for (int i = 0; i < 4; i++) {
+			String passwordBcrypt = passwordEncoder.encode(password);
+			System.out.println(passwordBcrypt);
+		}
+		
 	}
 }
