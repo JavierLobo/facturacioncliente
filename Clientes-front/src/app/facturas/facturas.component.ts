fcfed0ac169c5772776373cebe7ede5043082681
javierlobo <franciscojavier.lobo@gmail.com>
2020-11-16 07:52:58 +0100

    (DEV) Add autocomplete control

--- Clientes-front/src/app/facturas/facturas.component.ts
+++ Clientes-front/src/app/facturas/facturas.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {FormControl} from '@angular/forms';
+
+import { Observable } from 'rxjs';
+import { map, flatMap } from 'rxjs/operators';
 
 import { ClienteService } from '../clientes/cliente.service';
+import { FacturaService } from './services/factura.service';
 
 import { Factura }  from './models/factura';
+import { Producto }  from './models/producto';
 
 @Component({
   selector: 'app-facturas',
@@ -14,8 +20,13 @@ export class FacturasComponent implements OnInit {
   titulo: string = "Nueva Factura";
   factura: Factura = new Factura();
 
+  autoCompleteControl = new FormControl();
+  productos: string[] = ['One', 'Two', 'Three'];
+  productosFiltrados: Observable<Producto[]>;
+
   constructor(
       private clienteService: ClienteService,
+      private facturaService: FacturaService,
       private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -23,5 +34,17 @@ export class FacturasComponent implements OnInit {
       let clienteId = +params.get('clienteId');
       this.clienteService.getCliente(clienteId).subscribe(cliente => this.factura.cliente = cliente);
     });
+
+    this.productosFiltrados = this.autoCompleteControl.valueChanges
+      .pipe(
+        map(value => typeof value === 'string' ? value : value.nombre),
+        flatMap(value => value ? this._filter(value) : [])
+      );
+  }
+
+  private _filter(value: string): Observable<Producto[]> {
+    const filterValue = value.toLowerCase();
+
+    return this.facturaService.filtrarProductos(filterValue);
   }
 }
